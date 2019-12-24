865b64271305304868127a940c124530d81c079a
Sijuade Ajagunna <mahyor.sam@gmail.com>
2019-12-24 17:09:08 +0100

    add security features

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import logger from 'morgan';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xssFIlter from 'x-xss-protection';
 import { config } from 'dotenv';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
 import toursRouter from './routes/tourRoutes';
 import usersRouter from './routes/userRoutes';
 import OpError from './utils/errorClass';
@@ -11,11 +16,48 @@ config();
 const app = express();
 const staticPath = `${__dirname}/../public`;
 
+//GLOBAL MIDDLEWARE
+//SECURITY HEADERS
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(logger('dev'));
 }
 
-app.use(express.json());
+//RATE LIMITER
+const limiter = rateLimit({
+  max: 120,
+  windowMs: 60 * 60 * 1000,
+  message:
+    'We have noticed an unusual amount of request from this IP, please try again later'
+});
+
+app.use('/api', limiter);
+
+//BODY PARSER
+app.use(express.json({ limit: '10kb' }));
+
+//Data Sanitization
+//-against NoSQL query Injection
+app.use(mongoSanitize());
+//-against XSS
+app.use(xssFIlter());
+
+//FIX PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQunatity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+//STATIC FILES
 app.use(express.static(staticPath));
 
 app.use((req, res, next) => {
@@ -26,6 +68,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
+//ERROR HANDLER
 app.all('*', (req, res, next) => {
   next(new OpError(404, `Can't find ${req.originalUrl} on this server`));
 });
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,6 +22,18 @@ const generateToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = generateToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
     token,
